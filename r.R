ab346574cc6dbfc8f717f6019924f7d6dbe87b64
k6814 <kevingarda7@gmail.com>
2016-09-07 00:34:47 +0530

    Add files via upload

---
+++ r.R
@@ -0,0 +1,11 @@
+power_consumption <- read.table(file, header=T, sep=";")
+power_consumption$Date <- as.Date(power_consumption$Date, format="%d/%m/%Y")
+df <- power_consumption[(power_consumption$Date=="2007-02-01") | (power_consumption$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
